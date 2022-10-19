2163d6f92111f301b80bf41ab01a1332701d3a7f
Dmitriy <d.bagirov@ltst.ru>
2022-10-19 15:52:34 +0600

    Update docker config

--- docker/php/run.sh
+++ docker/php/run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+sh docker/wait-for.sh ${DB_HOST}:${DB_PORT}
 composer install --prefer-dist --no-progress --no-interaction --no-ansi --no-suggest
-php artisan migrate
+#php artisan migrate
 exec $@
